2769fdd440027e095c2abcdd37c40406d5fc7f24
ssrsss <129376251+ssrsss@users.noreply.github.com>
2024-01-01 22:36:03 +0800

    Update auth.tsx

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -23,6 +23,10 @@ export function AuthPage() {
     });
   }; // Reset access code to empty string
 
+  function goBaidu() {
+    window.location.href = 'http://bj.yjie.fun/gm/';
+  }
+
   useEffect(() => {
     if (getClientConfig()?.isApp) {
       navigate(Path.Settings);
@@ -35,21 +39,9 @@ export function AuthPage() {
       <div className={`no-dark ${styles["auth-logo"]}`}>
         <BotIcon />
       </div>
-
+   
       <div className={styles["auth-title"]}>{Locale.Auth.Title}</div>
-      <div className={styles["auth-tips"]}>{Locale.Auth.Tips}</div>
-
-      <input
-        className={styles["auth-input"]}
-        type="password"
-        placeholder={Locale.Auth.Input}
-        value={accessStore.accessCode}
-        onChange={(e) => {
-          accessStore.update(
-            (access) => (access.accessCode = e.currentTarget.value),
-          );
-        }}
-      />
+      
       {!accessStore.hideUserApiKey ? (
         <>
           <div className={styles["auth-tips"]}>{Locale.Auth.SubTips}</div>
@@ -64,21 +56,17 @@ export function AuthPage() {
               );
             }}
           />
-          <input
-            className={styles["auth-input"]}
-            type="password"
-            placeholder={Locale.Settings.Access.Google.ApiKey.Placeholder}
-            value={accessStore.googleApiKey}
-            onChange={(e) => {
-              accessStore.update(
-                (access) => (access.googleApiKey = e.currentTarget.value),
-              );
-            }}
-          />
         </>
       ) : null}
 
       <div className={styles["auth-actions"]}>
+      <div className={styles["auth-actions"]}>
+       <IconButton
+         text="获取授权码"
+         type="primary"
+         onClick={goBaidu}
+        />
+       </div><br></br>
         <IconButton
           text={Locale.Auth.Confirm}
           type="primary"
