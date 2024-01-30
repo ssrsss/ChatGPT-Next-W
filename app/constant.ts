a51e297a43683f2473c3c88178993524856d5451
ssrsss <129376251+ssrsss@users.noreply.github.com>
2024-01-30 17:06:33 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -118,15 +118,6 @@ export const KnowledgeCutOffDate: Record<string, string> = {
 };
 
 export const DEFAULT_MODELS = [
- {
-    name: "gemini-pro",
-    available: true,
-    provider: {
-      id: "openai",
-      providerName: "OpenAI",
-      providerType: "openai",
-    },
-  },
 {
     name: "bing",
     available: true,
