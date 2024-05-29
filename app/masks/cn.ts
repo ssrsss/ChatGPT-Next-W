553bba2917c772b52d3263cc1354151518bac99a
ssrsss <129376251+ssrsss@users.noreply.github.com>
2024-05-30 01:28:42 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -14,7 +14,7 @@ export const CN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "ChatGPT-3.5-插件专用模型",
+      model: "gpt-3.5-turbo-0125",
       temperature: 1,
       max_tokens: 15000,
       presence_penalty: 0,
